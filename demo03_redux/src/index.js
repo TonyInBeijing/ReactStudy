df5970cf257eb55fa1be6714e678d2c87f62051e
TonyInBeijing <1300565735@qq.com>
2020-09-14 17:11:52 +0800

    redux在react中的应用

--- demo03_redux/src/index.js
+++ demo03_redux/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import rootReducer from './redux/reducers/index';
 
-const store = createStore();
-
+const store = createStore(rootReducer);
 ReactDOM.render(
     <Provider store={store}>
         <App />
