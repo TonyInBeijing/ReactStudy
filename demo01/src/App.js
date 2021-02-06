4ffeac3b5ce2d04266726a830e3a51ed3dbc28f0
TonyInBeijing <1300565735@qq.com>
2021-02-06 22:49:39 +0800

    修改app.js

--- demo01/src/App.js
+++ demo01/src/App.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 
 class App extends Component {
     render() {
-        // 不用jsx
-        // const li1 = React.createElement('li', null, 'Hello World!');
-        // const li2 = React.createElement('li', null, 'Hello Tony!');
-        // const root = React.createElement('ul', { className: 'mylist' }, li1, li2);
         return (
             <ul className="my-list">
                 <li>Hello World!</li>
